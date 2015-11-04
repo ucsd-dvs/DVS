5d661cd64fcefd7db4da232c297ac8cd02713cae
zarni wang <zarnimng@gmail.com>
2015-11-04 07:28:21 -0800

    ports alert over to javafx and removes controlfx dependency

--- src/main/java/com/ucsd/globalties/dvs/core/ui/PhotoGridController.java
+++ src/main/java/com/ucsd/globalties/dvs/core/ui/PhotoGridController.java
@@ -4,13 +4,13 @@ import com.ucsd.globalties.dvs.core.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
-import org.controlsfx.control.action.Action;
-import org.controlsfx.dialog.Dialogs;
 
 import java.io.File;
 import java.net.URL;
@@ -96,13 +96,8 @@ public class PhotoGridController implements Initializable, ControlledScreen {
     @FXML
     private void goToDetectGrid(ActionEvent event) {
         if (vFilePath == null || hFilePath == null) {
-            //controlfx dialogs is deprecated since it will be merged into java
-            //planned for 8u40, sometime March 2015
-            Action alert = Dialogs.create()
-                    .owner(root.getScene().getWindow())
-                    .title("Alert!")
-                    .message("Please select the appropriate images.")
-                    .showError();
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please select the appropriate images", ButtonType.CLOSE);
+            alert.showAndWait();
         } else {
             rootViewController.getController().setPatientPhotos(hFilePath, vFilePath);
             navigationController.setScreen(Main.detectGridID);
