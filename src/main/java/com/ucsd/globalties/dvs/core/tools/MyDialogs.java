66c2f3fab1eadb713be4cee36c6e80c548494067
zarni wang <zarnimng@gmail.com>
2015-11-09 11:01:09 -0800

    Adds exception handling to excel export

--- src/main/java/com/ucsd/globalties/dvs/core/tools/MyDialogs.java
+++ src/main/java/com/ucsd/globalties/dvs/core/tools/MyDialogs.java
@@ -1,7 +1,11 @@
 package com.ucsd.globalties.dvs.core.tools;
 
-/**
- * Created by zwang on 11/9/15.
- */
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
 public class MyDialogs {
+    public static void showError(String msg) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, msg, ButtonType.CLOSE);
+        alert.showAndWait();
+    }
 }
