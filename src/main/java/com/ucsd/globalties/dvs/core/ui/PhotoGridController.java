c9918d846d672c1577514b964456f509b2ba9293
Heather <hhua729@gmail.com>
2016-02-16 14:02:39 -0800

    fixed

--- src/main/java/com/ucsd/globalties/dvs/core/ui/PhotoGridController.java
+++ src/main/java/com/ucsd/globalties/dvs/core/ui/PhotoGridController.java
@@ -145,26 +145,15 @@ public class PhotoGridController implements Initializable, ControlledScreen {
          * 5) What do we do w/ existing pictures in folder?
          */
 
-<<<<<<< HEAD
         String folder = getCurrentTimeStamp();
         System.out.println("This is our folder: " + folder);
         Path dir_path = Paths.get(System.getProperty("user.home") + "/Desktop/" + folder);
         WatchDir watcher = new WatchDir(dir_path, false);
 
-//        private String hFilePath, vFilePath;
-=======
-        Path dir_path = Paths.get(System.getProperty("user.home") + "/Pictures/test");
-        watcher = new WatchDir(dir_path, false);
-
         // Set horizontal picture
->>>>>>> 28e8c9e121ca0dacb6b1eb220493164815174679
         hStrProperty = new SimpleStringProperty();
         hStrProperty.bind(watcher.messageProperty());
-        hStrProperty.addListener((observable, oldValue, newValue) -> {
-            imgHoriz.setImage(new Image("file:///" + newValue));
-            hFilePath = newValue;
 
-<<<<<<< HEAD
         //Set vertical picture
         hStrProperty.addListener(new ChangeListener<String>() {
             @Override
@@ -180,42 +169,8 @@ public class PhotoGridController implements Initializable, ControlledScreen {
                     imgVert.setRotate(-90);
                 }
             }
-=======
-            // TODO check if image is good before unbinding
-            System.out.println("Inside hStrProperty listener");
-            System.out.println(oldValue + " vs. " + newValue);
-            System.out.println(hStrProperty.isBound());
-            System.out.println(vStrProperty.isBound());
-            System.out.println();
 
-            hStrProperty.unbind();
-            vStrProperty.bind(watcher.messageProperty());
->>>>>>> 28e8c9e121ca0dacb6b1eb220493164815174679
         });
-
-        // Set vertical picture
-        vStrProperty = new SimpleStringProperty();
-        vStrProperty.addListener(((observable, oldValue, newValue) -> {
-            // There has to be a null check b/c the listener gets signaled
-            // as soon as vStrProperty is bound. So a signal gets fired as
-            // soon as the horizontal picture is taken and needs to be
-            // ignored here.
-            if (oldValue != null) {
-                imgVert.setImage(new Image("file:///" + newValue));
-                imgVert.setRotate(-90);
-                vFilePath = newValue;
-
-                // TODO check if image is good before unbinding
-                System.out.println("Inside vStrProperty listener");
-                System.out.println(oldValue + " vs. " + newValue);
-                System.out.println(hStrProperty.isBound());
-                System.out.println(vStrProperty.isBound());
-                System.out.println();
-
-                vStrProperty.unbind();
-//                rootViewController.getNextButton().setDisable(false);
-            }
-        }));
 
         //Run watcher on individual thread
         Thread th = new Thread(watcher);
@@ -240,54 +195,20 @@ public class PhotoGridController implements Initializable, ControlledScreen {
      * JavaFX ignores orientation of image so vertical images get loaded as horizontal
      * and has to be rotated
      */
-<<<<<<< HEAD
+
     @FXML
     private void selectVerticalPicture(ActionEvent event) {
-=======
-//    @FXML
-//    private void selectVerticalPicture(ActionEvent event) {
->>>>>>> 28e8c9e121ca0dacb6b1eb220493164815174679
-//        File dir = new File(System.getProperty("user.dir")+"/pics"); // use this for testing
-////        File dir = new File(System.getProperty("user.home")); // use this for production
-//        fileChooser.setInitialDirectory(dir.getAbsoluteFile());
-//        File file = fileChooser.showOpenDialog(root.getScene().getWindow());
-//        if (file != null) {
-//            fileChooser.setInitialDirectory(file.getParentFile());
-//            vFilePath = file.getAbsolutePath();
-//            imgVert.setImage(new Image("file:///" + vFilePath));
-//            imgVert.setRotate(-90);
-//        }
-<<<<<<< HEAD
+
         vFilePath = null;
         imgVert.setImage(null);
     }
 
     @FXML
     private void selectHorizontalPicture(ActionEvent event) {
-=======
-//    }
-//
-//    @FXML
-//    private void selectHorizontalPicture(ActionEvent event) {
->>>>>>> 28e8c9e121ca0dacb6b1eb220493164815174679
-//        File dir = new File(System.getProperty("user.dir")+"/pics"); // use this for testing
-////        File dir = new File(System.getProperty("user.home")); // use this for production
-//        fileChooser.setInitialDirectory(dir.getAbsoluteFile());
-//        File file = fileChooser.showOpenDialog(root.getScene().getWindow());
-//        if (file != null) {
-//            fileChooser.setInitialDirectory(file.getParentFile());
-//            hFilePath = file.getAbsolutePath();
-//            imgHoriz.setImage(new Image("file:///" + hFilePath));
-////            System.out.println(hFilePath);
-//        }
-<<<<<<< HEAD
         hFilePath = null;
         imgHoriz.setImage(null);
     }
 
-=======
-//    }
->>>>>>> 28e8c9e121ca0dacb6b1eb220493164815174679
     private void goToInputGrid() {
         if (watcher != null) {
             if (!watcher.cancel()) {
